f39757882eadf333c81d93bc285c5a4cbdfdc60e
1813625642 - Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-01-09 00:16:39 +0600

    admin

---
+++ admin/admin.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/admin.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    <!-- HEADER start here -->
+    <header class="topbar">
+        <div class="container flex justify-between items-center">
+            <div class="icons">
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+                <a href=""><i class="fab fa-google"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fas fa-search"></i></a>
+            </div>
+            <div class="auth flex items-center">
+                <div>
+                    <i class="fas fa-users"></i>
+                    <a href="./index.html">Log out</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- HEADER ends here -->
+
+    <!-- Navbar starts here -->
+    <nav>
+        <!-- top-nav start -->
+        <div class="top">
+            <div class="container flex justify-between">
+                <div class="contact flex items-center">
+                    <i class="fas fa-phone-alt"></i>
+                    <div>
+                        <h5>Call US: (+880) 17 93798 719</h5>
+                        <h6>E-mail: masruf.jaman@northsouth.edu</h6>
+                    </div>
+                </div>
+                <!-- logo -->
+                <div class="branding">
+                    <img src="./images/logo.png" alt="" />
+                </div>
+                <div class="time flex items-center">
+                    <i class="far fa-clock"></i>
+                    <div>
+                        <h5>Working Hours:</h5>
+                        <h6>Mon - Sat (8.00am - 10.00pm)</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- top-nav end -->
+    </nav>
+    <!-- Navbar ends here -->
+
+    <!-- admin -->
+    <a class="mobile">MENU</a>
+
+    <div id="container">
+        <div class="sidebar">
+            <ul id="nav">
+                <li>
+                    <span>
+                        <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" />
+                    </span>
+                    <a class="selected" href="#">Dashboard</a>
+                </li>
+                <li>
+                    <span>
+                        <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" />
+                    </span>
+                    <a href="#">Product Details</a>
+                </li>
+                <li>
+                    <span>
+                        <img src="http://www.webhostingreviewsbynerds.com/wp-content/plugins/rss-poster/cache/e17b1_fnal-570x456.png" width="32" height="32" /> </span><a href="#">Sale Details</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="content">
+            <h1>Dashboard</h1>
+            <p>Here you can do stuff</p>
+
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">Lorem nes stuf</div>
+            </div>
+
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">Lorem nes stuf</div>
+            </div>
+
+            <div id="box">
+                <div class="box-top">News</div>
+                <div class="box-panel">Lorem nes stuf</div>
+            </div>
+        </div>
+    </div>
+    <!-- admin -->
+
+    <!-- Footer Start -->
+    <div class="footer">
+        <div class="container">
+            <div class="row flex">
+                <div class="footer-col-1">
+                    <h3>Download Our App</h3>
+                    <p>Download App for Android & ios mobile phone.</p>
+                    <div class="app-logo">
+                        <img src="/images/play-store.png" alt="" />
+                        <img src="/images/app-store.png" alt="" />
+                    </div>
+                </div>
+                <div class="footer-col-2">
+                    <img src="/images/Logo.png" alt="" />
+                    <p>
+                        Our Purpose Is To Sustainably Make the pleasure and Benefits of
+                        Sports Accessible to the Many.
+                    </p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Blog Post</li>
+                        <li>Return Policy</li>
+                        <li>join Affiliate</li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>YouTube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+
+            <p class="coyright">
+                <i class="fa fa-copyright"></i> Copyright 2020 - 2020 |<strong style="color: #eebf00">StormBringers</strong>| All Right Reserved
+            </p>
+        </div>
+    </div>
+    <!-- Footer end -->
+    <!-- #container -->
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+    <script src="./js/admin.js"></script>
+</body>
+
+</html>
---
+++ admin/connection.inc.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","", "project_group_5_cse311l_section_02");
